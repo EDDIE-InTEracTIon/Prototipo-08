b693d2a974630ac929a748db1ec45b336e82fcd0
Criscove <ccontrerasv@gmail.com>
2020-01-15 19:07:31 -0300

    Agregar archivos de proyecto.

---
+++ AugmentedReadingApp/ProjectionScreenActivity.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using ModuloProcesamientoImagenes;
+using ModuloVisualizacionDatos;
+
+namespace AugmentedReadingApp
+{
+    public partial class ProjectionScreenActivity : Form
+    {
+        public int CameraNumber;
+        InteractionCoordinator originalForm;
+       
+        bool markPoint = true;
+        public ColorRecognition recTxt;
+        
+        public HighlightTool Highlight;
+
+        public ProjectionScreenActivity(InteractionCoordinator incomingForm)
+        {
+            
+            originalForm = incomingForm;
+            InitializeComponent();
+            
+            pictureBox3.Image = Image.FromFile("x_mark_red_circle.png");
+            pictureBox4.Image = Image.FromFile("x_mark_red_circle.png");
+            pictureBox5.Image = Image.FromFile("x_mark_red_circle.png");
+
+            markPoint = false;
+
+
+            Highlight = new HighlightTool(30, 517, 400)
+            {
+                Name = "HighlightBox",
+                BackColor = Color.FromArgb(255, 255, 255),
+                
+                Location = new Point(240, 100),
+
+
+            };
+            this.Controls.Add(Highlight);
+
+            Highlight.NumClicks += ButtonAction;
+
+        }
+
+
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+        // Load del Form
+        }
+      
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (markPoint)
+            {
+
+
+                pictureBox3.Image = Image.FromFile("x_mark_red_circle.png");
+                pictureBox4.Image = Image.FromFile("x_mark_red_circle.png");
+                pictureBox5.Image = Image.FromFile("x_mark_red_circle.png");
+
+                markPoint = false;
+            }
+            else
+            {
+                pictureBox3.Image = null;
+                pictureBox4.Image = null;
+                pictureBox5.Image = null;
+                markPoint = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (Highlight.HighLightOn)
+            {
+
+                originalForm.CaptureTxt();
+
+                try
+                {
+                    textBox1.Text = OCRProcess.TransformImage(originalForm.recTxt.TextImage.Bitmap);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("OCRProcess Resaltado: " + ex.Message);
+                }
+
+            }
+            else
+            {
+                originalForm.CaptureImage();
+
+                try
+                {
+                    textBox1.Text = OCRProcess.TransformImage(originalForm.RectangleImage.Bitmap);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("OCRProcess rectangulo: " + ex.Message);
+                }
+            }
+        }
+
+ 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            if (Highlight.HighLightOn)
+            {
+
+                button3.Text = "TEXT/IMAGE";
+                button3.BackColor = default(Color);
+                Highlight.HighLightOn = false;
+
+            }
+            else
+            {
+                button3.BackColor = Color.Gray;
+                button3.Text = "HIGHLIGHT";
+                Highlight.HighLightOn = true;
+            }
+
+
+        }
+
+        private void ButtonAction()
+        {
+
+            if (Highlight.HighLightOn)
+            {
+
+                originalForm.CaptureTxt();
+
+                try
+                {
+                    textBox1.Text = OCRProcess.TransformImage(originalForm.recTxt.TextImage.Bitmap);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("OCRProcess Resaltado: " + ex.Message);
+                }
+
+            }
+            else
+            {
+                if (originalForm.plugin.AutoCamCapture)
+                {
+                    originalForm.CaptureImage();
+
+                    try
+                    {
+                        textBox1.Text = OCRProcess.TransformImage();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("OCRProcess rectangulo: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            
+            float llx = (Highlight.normRect.Left) * originalForm.documentoSyn.rectPage.Right;
+            float lly = (1 - Highlight.normRect.Bottom ) * originalForm.documentoSyn.rectPage.Top - (Highlight.normRect.Top) * originalForm.documentoSyn.rectPage.Top;
+            float urx = (Highlight.normRect.Left) * originalForm.documentoSyn.rectPage.Right + (Highlight.normRect.Right) * originalForm.documentoSyn.rectPage.Right;
+            float ury = (1 - Highlight.normRect.Bottom) * originalForm.documentoSyn.rectPage.Top;
+            originalForm.documentoSyn.SaveAnno(llx, lly, urx, ury);
+            Highlight.GetRectangles(originalForm.documentoSyn.listaRectangulos);
+        }
+    }
+}
+
---
+++ AugmentedReadingApp/ProjectionScreenActivity.designer.cs
@@ -0,0 +1,141 @@
+﻿namespace AugmentedReadingApp
+{
+    partial class ProjectionScreenActivity
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 330);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 215);
+            this.textBox1.TabIndex = 6;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(270, 20);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(60, 60);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 86;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(270, 570);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(60, 60);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 87;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(770, 570);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(60, 60);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 88;
+            this.pictureBox5.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 100);
+            this.button1.TabIndex = 89;
+            this.button1.Text = "Marcas ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 118);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(200, 100);
+            this.button3.TabIndex = 91;
+            this.button3.Text = "TEXT/IMAGE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1059, 73);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 92;
+            this.button2.Text = "Sync";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // ProjectionScreenActivity
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "ProjectionScreenActivity";
+            this.Text = "AR";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+    }
+}
